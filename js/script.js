2f134a15ebdf518bc1b79618fe980f35187ed66f
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-10 15:05:15 +0100

    Inizializzazione

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Distanza in km//
+const distanceKm=parseInt(prompt('Inserisci la distanza che vuoi percorrere in Kilometri'));
+console.log(distanceKm);
+
+//Età viaggiatore//
+const userAge=parseInt(prompt('Inserisci la tua età'));
+console.log(userAge)
+
